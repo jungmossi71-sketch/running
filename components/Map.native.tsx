0f74adaa25de2e925a3eecab1a886233d5c6f8b2
JungKeunSoo <iwouldlike119@gmail.com>
2026-03-31 20:39:36 +0900

    google map api key update

--- components/Map.native.tsx
+++ components/Map.native.tsx
@@ -8,16 +8,12 @@ export default function Map({ route, lineColor = '#39FF14' }: { route: {latitude
   useEffect(() => {
     if (route.length > 0 && mapRef.current) {
       const lastCoord = route[route.length - 1];
-      
-      // 첫 번째 위치가 잡혔을 때 또는 이동 중에 화면 중앙 추적
-      // 원한다면 isFirstLocation.current 조건으로 처음에만 튀게 할 수도 있음
       mapRef.current.animateToRegion({
         latitude: lastCoord.latitude,
         longitude: lastCoord.longitude,
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       }, 1000);
-      
       if (isFirstLocation.current) {
         isFirstLocation.current = false;
       }
@@ -25,7 +21,7 @@ export default function Map({ route, lineColor = '#39FF14' }: { route: {latitude
   }, [route]);
 
   return (
-    <MapView 
+    <MapView
       ref={mapRef}
       provider={PROVIDER_GOOGLE}
       style={{ width: '100%', height: '100%' }}
